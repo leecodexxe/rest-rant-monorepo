18ac219160ddb94f64e25f1a66880d72ff8fcf13
leecodexxe~ <lee4works@gmail.com>
2023-09-28 20:40:27 -0700

    change for iam-4

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
-const cookieSession = require('cookie-session')
 const cors = require('cors')
 const app = express();
 const cookieSession = require('cookie-session')
